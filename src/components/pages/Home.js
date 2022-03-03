661035e11718dd1057842d4cc9bfa5c240728735
Pawan <pawandeepk@my.com>
2022-03-03 20:19:30 +0000

    connected back end api to front end

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,38 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { render } from 'react-dom';
 import Card from '../generic/Card';
 import Assignment from './Assignment';
 
-function Home() {
-    //dummy data ------------------
-   const upcomingAssignments = [{
-        title: "Software Development Practice EPIC 2",
-        code: "CI6125",
-        dueDate: "04/11/2021",
-        description: "Based  on  the  shared  and  individual  outcomes  from  the  workshop  2  and  workshop  3,  do  a write-up  covering  the  following  5  sections.  As  in  Epic  1,  we  will  be  creating  an  ePortfolio GoogleSite in TB2 based on these sections, and other content from subsequent Epics.",
-    },
-    {
-        title: "Mobile Application Development",
-        code: "CI6330",
-        dueDate: "10/01/2022",
-        description: "This module provides a foundation for careers in mobile application development: mobile is becoming increasingly ubiquitous and the skills taught also have applications in UX, web development and software engineering in general. Although there are no prerequisites, it is assumed that students have acquired a general familiarity with programming and software development principles through their previous study."
-    },
-    ];
+const BASE_URL = "http://localhost:5000";
+const GET_MODULES = '/getmodules'
 
-    const pastAssignments = [{
-        title: "Software Development Practice EPIC 1",
-        code: "CI6125",
-        dueDate: "20/10/2021",
-        description: "Based  on  the  shared  and  individual  outcomes  from  the  workshop  2  and  workshop  3,  do  a write-up  covering  the  following  5  sections.  As  in  Epic  1,  we  will  be  creating  an  ePortfolio GoogleSite in TB2 based on these sections, and other content from subsequent Epics.",
-    },
-    {
-        title: "Programming III",
-        code: "CI6115",
-        dueDate: "15/10/2021",
-        description: "Based  on  the  shared  and  individual  outcomes  from  the  workshop  2  and  workshop  3,  do  a write-up  covering  the  following  5  sections.  As  in  Epic  1,  we  will  be  creating  an  ePortfolio GoogleSite in TB2 based on these sections, and other content from subsequent Epics.",
-    },
-    ];
-   
+function Home() {
+
+    let data;
+
+    const fetchModules = async () => {
+        try {
+            const response = await fetch(`${BASE_URL}${GET_MODULES}`)
+            data = await response.json()
+        } catch (error) {
+            throw new Error('Error fetching modules', error)
+        }
+        return data;
+    }
+
+    useEffect(() => {
+        fetchModules()
+    }, [])
+
     //properties ---------------------
     //hooks ---------------------
     //context ---------------------
@@ -42,33 +33,18 @@ function Home() {
              <section>
                 <h1>My Upcoming Group Assignments</h1>
                   {
-                      upcomingAssignments.map(assignmentInfo => {
+                      data.map(dataInfo => {
                     return(
                         <div>
                         <Card
-                            title={assignmentInfo.title + " (" + assignmentInfo.code + ")"}>
-                            <span>{"Due: " + assignmentInfo.dueDate}<br/></span>
-                            <span>{assignmentInfo.description}</span>
+                            title={dataInfo.moduleName + " (" + dataInfo.moduleCode + ")"}>
+                            <span>{"Lorem Ipsum"}<br/></span>
+                            <span>{"Lorem Ipsum"}</span>
                         </Card>
                         </div>
                     );
                    })
                   }
-
-                <h1>My Past Group Assignments</h1>
-                {
-                    pastAssignments.map(pastAssignmentInfo => {
-                        return(
-                            <div>
-                                <Card
-                                    title={pastAssignmentInfo.title + " (" + pastAssignmentInfo.code + ")"}>
-                            <span>{"Due: " + pastAssignmentInfo.dueDate}<br/></span>
-                            <span>{pastAssignmentInfo.description}</span>
-                            </Card>
-                            </div>
-                        )
-                    })
-                }
                 
              </section>
          );
